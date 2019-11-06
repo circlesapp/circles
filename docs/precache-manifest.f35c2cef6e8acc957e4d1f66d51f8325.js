dc5581d358faed5cbc62d19569ce8b5b9b94fadb
heptacode <admin@hyunwoo.kim>
2019-11-07 03:23:48 +0900

    Update build

--- docs/precache-manifest.f35c2cef6e8acc957e4d1f66d51f8325.js
+++ docs/precache-manifest.f35c2cef6e8acc957e4d1f66d51f8325.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "298c58fe31da4467de95",
+    "revision": "e94f34df2f3204a1b380",
     "url": "/FrontEnd/css/app.83bdf58c.css"
   },
   {
-    "revision": "298c58fe31da4467de95",
-    "url": "/FrontEnd/js/app.fb32d366.js"
+    "revision": "e94f34df2f3204a1b380",
+    "url": "/FrontEnd/js/app.a3efc450.js"
   },
   {
     "revision": "d3005ab17de86ce9cc91",
@@ -16,6 +16,10 @@ self.__precacheManifest = [
     "url": "/FrontEnd/img/circles_logo.4ebe0f3d.png"
   },
   {
+    "revision": "d10ab385fbea9101af7f13e5e2f8e968",
+    "url": "/FrontEnd/fonts/Avenir Black.d10ab385.ttf"
+  },
+  {
     "revision": "9ee5085e37e8ac9dd4aa44813d9b924a",
     "url": "/FrontEnd/fonts/NanumSquareEB.9ee5085e.ttf"
   },
@@ -24,28 +28,28 @@ self.__precacheManifest = [
     "url": "/FrontEnd/fonts/NanumSquareL.f2a56a3b.ttf"
   },
   {
-    "revision": "d10ab385fbea9101af7f13e5e2f8e968",
-    "url": "/FrontEnd/fonts/Avenir Black.d10ab385.ttf"
+    "revision": "284774d9e7fb6fd99446fbd0ea875ff2",
+    "url": "/FrontEnd/fonts/NanumSquareB.284774d9.ttf"
   },
   {
     "revision": "b0dcfde6dd89f2990b16ce6ac6808533",
     "url": "/FrontEnd/fonts/NanumSquareR.b0dcfde6.ttf"
   },
   {
-    "revision": "284774d9e7fb6fd99446fbd0ea875ff2",
-    "url": "/FrontEnd/fonts/NanumSquareB.284774d9.ttf"
-  },
-  {
-    "revision": "b327dbacddc89ed3081caf9dff3f4e87",
+    "revision": "764b70f0adf780b36b44e1dfa56bb2d3",
     "url": "/FrontEnd/index.html"
-  },
-  {
-    "revision": "866b22abb1e60882692d32799f0cebbc",
-    "url": "/FrontEnd/circles_banner.png"
   },
   {
     "revision": "da9ce3d3b330f8f7b9558cdbb7bd0b54",
     "url": "/FrontEnd/logo_192.png"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/FrontEnd/robots.txt"
+  },
+  {
+    "revision": "866b22abb1e60882692d32799f0cebbc",
+    "url": "/FrontEnd/circles_banner.png"
   },
   {
     "revision": "353cccf6ccb3b25cc7bf478a6858fe18",
@@ -54,9 +58,5 @@ self.__precacheManifest = [
   {
     "revision": "604fbde1ab75de02e22fb2c57406dbbe",
     "url": "/FrontEnd/service-worker.js"
-  },
-  {
-    "revision": "735ab4f94fbcd57074377afca324c813",
-    "url": "/FrontEnd/robots.txt"
   }
 ];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/FrontEnd/precache-manifest.641b2db4984794e7ab1fa289c70f11ff.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("/FrontEnd/precache-manifest.f35c2cef6e8acc957e4d1f66d51f8325.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 if (workbox) {
 	console.log(`Workbox is loaded`);
