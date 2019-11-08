3cf2fe82083bfba0f49b94b838439cacb541f641
Andy0414 <pjh8667@naver.com>
2019-11-08 20:05:41 +0900

    UPDATE 404

--- src/views/Page404.vue
+++ src/views/Page404.vue
@@ -1,6 +1,5 @@
 <template>
-	<div class="page404">
-	</div>
+	<div class="page404">404 NOT FOUND</div>
 </template>
 
 <script lang="ts">
@@ -9,4 +8,31 @@ export default Vue.extend({});
 </script>
 
 <style>
+.page404 {
+	font-family: "NanumSquareEB";
+	font-size: 100px;
+
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	background-color: #538fff;
+	color: white;
+
+    animation: notfound 2s infinite;
+}
+@keyframes notfound {
+	0% {
+		background-color: #538fff;
+		color: white;
+	}
+	50% {
+		background-color: white;
+		color: #538fff;
+	}
+	100% {
+		background-color: #538fff;
+		color: white;
+	}
+}
 </style>
