0a6610395daad0934860380907bf829ef350410f
Andy0414 <pjh8667@naver.com>
2019-11-24 22:01:36 +0900

    UPDATE service worker event test

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,13 +1,6 @@
 if (workbox) {
 	console.log(`Workbox is loaded`);
 
-	self.addEventListener("push", e => {
-		console.log("self outline push : ", e);
-	});
-	addEventListener("push", e => {
-		console.log("outline push : ", e);
-	});
-
 	workbox.precaching.precacheAndRoute(self.__precacheManifest);
 } else {
 	console.log(`Workbox didn't load`);
