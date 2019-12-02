b2f943c34ab094d8307a53da81e2fff8136a312b
Andy0414 <pjh8667@naver.com>
2019-12-03 08:45:35 +0900

    UPDATE sw

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -40,7 +40,7 @@ function startNotification() {
 	});
 }
 if (process.env.NODE_ENV === "production") {
-	register(`${process.env.BASE_URL}sw-dev5.js`, {
+	register(`${process.env.BASE_URL}sw.js`, {
 		ready() {
 			console.log("App is being served from cache by a service worker.\n" + "For more details, visit https://goo.gl/AFskqB");
 		},
