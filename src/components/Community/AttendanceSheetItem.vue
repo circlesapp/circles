9b96d38f64bfd5269fd97aba823754c8c9fe6c89
heptacode <admin@hyunwoo.kim>
2019-12-22 17:50:27 +0900

    Add Button Styles & Change State Descriptions

--- src/components/Community/AttendanceSheetItem.vue
+++ src/components/Community/AttendanceSheetItem.vue
@@ -5,25 +5,25 @@
 		<td>{{ data.role }}</td>
 		<td>
 			<div class="attendanceSheetItem__state" @click="$emit('changeState', { id: data._id, day: 0 })">
-				<button v-if="data.attendances[dates[0].date].state == 0" class="state__0">결석</button>
-				<button v-else-if="data.attendances[dates[0].date].state == 1" class="state__1">출석</button>
-				<button v-else-if="data.attendances[dates[0].date].state == 2" class="state__2">지각</button>
+				<button v-if="data.attendances[dates[0].date].state == 0" class="state__0">출석</button>
+				<button v-else-if="data.attendances[dates[0].date].state == 1" class="state__1">지각</button>
+				<button v-else-if="data.attendances[dates[0].date].state == 2" class="state__2">결석</button>
 			</div>
 			<textarea v-model="data.attendances[dates[0].date].comment"></textarea>
 		</td>
 		<td>
 			<div class="attendanceSheetItem__state" @click="$emit('changeState', { id: data._id, day: 1 })">
-				<button v-if="data.attendances[dates[1].date].state == 0" class="state__0">결석</button>
-				<button v-else-if="data.attendances[dates[1].date].state == 1" class="state__1">출석</button>
-				<button v-else-if="data.attendances[dates[1].date].state == 2" class="state__2">지각</button>
+				<button v-if="data.attendances[dates[1].date].state == 0" class="state__0">출석</button>
+				<button v-else-if="data.attendances[dates[1].date].state == 1" class="state__1">지각</button>
+				<button v-else-if="data.attendances[dates[1].date].state == 2" class="state__2">결석</button>
 			</div>
 			<textarea v-model="data.attendances[dates[1].date].comment"></textarea>
 		</td>
 		<td>
 			<div class="attendanceSheetItem__state" @click="$emit('changeState', { id: data._id, day: 2 })">
-				<button v-if="data.attendances[dates[2].date].state == 0" class="state__0">결석</button>
-				<button v-else-if="data.attendances[dates[2].date].state == 1" class="state__1">출석</button>
-				<button v-else-if="data.attendances[dates[2].date].state == 2" class="state__2">지각</button>
+				<button v-if="data.attendances[dates[2].date].state == 0" class="state__0">출석</button>
+				<button v-else-if="data.attendances[dates[2].date].state == 1" class="state__1">지각</button>
+				<button v-else-if="data.attendances[dates[2].date].state == 2" class="state__2">결석</button>
 			</div>
 			<textarea v-model="data.attendances[dates[2].date].comment"></textarea>
 		</td>
@@ -50,15 +50,44 @@ export default Vue.extend({
 }
 .attendanceSheetItem__state button {
 	border: none;
+	border-radius: 7px;
+	padding: 5px 15px;
+
+	font-size: 15px;
+	color: white;
+
+	transition: 0.2s;
 	cursor: pointer;
 }
 .attendanceSheetItem__state .state__0 {
-	background: red;
+	background: #fe0200;
+}
+.attendanceSheetItem__state .state__0:hover {
+	background: #fe0200d1;
 }
 .attendanceSheetItem__state .state__1 {
-	background: green;
+	background: #21a564;
+}
+.attendanceSheetItem__state .state__1:hover {
+	background: #21a564d1;
 }
 .attendanceSheetItem__state .state__2 {
-	background: yellow;
+	background: #fe9a00;
+}
+.attendanceSheetItem__state .state__2:hover {
+	background: #fe9a00d1;
+}
+
+.attendanceSheetItem textarea {
+	min-width: 200px;
+	min-height: 40px;
+
+	border: none;
+	box-shadow: 0 2px 5px rgba(0, 0, 0, 0.3);
+	transition: 0.2s;
+}
+.attendanceSheetItem textarea:hover {
+	border: none;
+	box-shadow: 0 2px 8px rgba(0, 0, 0, 0.3);
 }
 </style>
