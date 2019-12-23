27ab27dcefec129f95c61939864b1f50f0b6ad18
heptacode <admin@hyunwoo.kim>
2019-12-23 18:48:50 +0900

    Add Lazy Loading Imports

--- src/router.ts
+++ src/router.ts
@@ -1,33 +1,6 @@
 import Vue from "vue";
 import Router from "vue-router";
 import VueAnalytics from "vue-analytics";
-import Home from "./views/Home.vue";
-import Profile from "./views/Profile.vue";
-import Search from "./views/Search.vue";
-import Login from "./views/Login.vue";
-import Register from "./views/Register.vue";
-import PasswordChange from "./views/PasswordChange.vue";
-import Sitemap from "./views/Sitemap.vue";
-import Page from "./views/Page.vue";
-import Site from "./views/Site.vue";
-import Community from "./views/Community.vue";
-import Timeline from "./views/Page/Timeline.vue";
-import Awards from "./views/Page/Awards.vue";
-import Members from "./views/Page/Members.vue";
-import Budgets from "./views/Page/Budgets.vue";
-import Applicant from "./views/Page/Applicant.vue";
-import ApplicantMain from "./views/Page/Applicant/Applicant.vue";
-import Application from "./views/Page/Applicant/Application.vue";
-import Recorder from "./views/Page/Applicant/Recorder.vue";
-import Result from "./views/Page/Applicant/Result.vue";
-import Editor from "./views/Community/Editor.vue";
-import EditClub from "./views/Community/EditClub.vue";
-import AttendanceSheet from "./views/Community/AttendanceSheet.vue";
-import Calendar from "./views/Community/Calendar.vue";
-import CommunityMembers from "./views/Community/Members.vue";
-import CommunityApplication from "./views/Community/Application.vue";
-import Interview from "./views/Community/Interview.vue";
-import Page404 from "./views/Page404.vue";
 
 Vue.use(Router);
 
@@ -38,60 +11,60 @@ const router = new Router({
 		{
 			path: "/",
 			name: "home",
-			component: Home
+			component: () => import("./views/Home.vue")
 		},
 		{
 			path: "/profile",
 			name: "profile",
-			component: Profile
+			component: () => import("./views/Profile.vue")
 		},
 		{
 			path: "/search",
 			name: "search",
-			component: Search
+			component: () => import("./views/Search.vue")
 		},
 		{
 			path: "/login",
 			name: "login",
-			component: Login
+			component: () => import("./views/Login.vue")
 		},
 		{
 			path: "/register",
 			name: "register",
-			component: Register
+			component: () => import("./views/Register.vue")
 		},
 		{
 			path: "/passwordchange",
 			name: "passwordchange",
-			component: PasswordChange
+			component: () => import("./views/PasswordChange.vue")
 		},
 		{
 			path: "/sitemap",
 			name: "sitemap",
-			component: Sitemap
+			component: () => import("./views/Sitemap.vue")
 		},
 		{
 			path: "/:club",
 			name: "site",
-			component: Site
+			component: () => import("./views/Site.vue")
 		},
 		{
 			path: "/:club/page",
 			name: "page",
-			component: Page,
+			component: () => import("./views/Page.vue"),
 			children: [
-				{ path: "timeline", name: "page/timeline", component: Timeline },
-				{ path: "awards", name: "page/awards", component: Awards },
-				{ path: "members", name: "page/members", component: Members },
-				{ path: "budgets", name: "page/budgets", component: Budgets },
+				{ path: "timeline", name: "page/timeline", component: () => import("./views/Page/Timeline.vue") },
+				{ path: "awards", name: "page/awards", component: () => import("./views/Page/Awards.vue") },
+				{ path: "members", name: "page/members", component: () => import("./views/Page/Members.vue") },
+				{ path: "budgets", name: "page/budgets", component: () => import("./views/Page/Budgets.vue") },
 				{
 					path: "applicant",
-					component: Applicant,
+					component: () => import("./views/Page/Applicant.vue"),
 					children: [
-						{ path: "", name: "page/applicant/main", component: ApplicantMain },
-						{ path: "application", name: "page/applicant/application", component: Application },
-						{ path: "recorder", name: "page/applicant/recorder", component: Recorder },
-						{ path: "result", name: "page/applicant/result", component: Result }
+						{ path: "", name: "page/applicant/main", component: () => import("./views/Page/Applicant/Applicant.vue") },
+						{ path: "application", name: "page/applicant/application", component: () => import("./views/Page/Applicant/Application.vue") },
+						{ path: "recorder", name: "page/applicant/recorder", component: () => import("./views/Page/Applicant/Recorder.vue") },
+						{ path: "result", name: "page/applicant/result", component: () => import("./views/Page/Applicant/Result.vue") }
 					]
 				}
 			]
@@ -99,21 +72,21 @@ const router = new Router({
 		{
 			path: "/:club/community",
 			name: "community",
-			component: Community,
+			component: () => import("./views/Community.vue"),
 			children: [
-				{ path: "editor", name: "community/editor", component: Editor },
-				{ path: "editclub", name: "community/editclub", component: EditClub },
-				{ path: "attendanceSheet", name: "community/attendanceSheet", component: AttendanceSheet },
-				{ path: "calendar", name: "community/calendar", component: Calendar },
-				{ path: "members", name: "community/members", component: CommunityMembers },
-				{ path: "application", name: "community/application", component: CommunityApplication },
-				{ path: "interview", name: "community/interview", component: Interview }
+				{ path: "editor", name: "community/editor", component: () => import("./views/Community/Editor.vue") },
+				{ path: "editclub", name: "community/editclub", component: () => import("./views/Community/EditClub.vue") },
+				{ path: "attendanceSheet", name: "community/attendanceSheet", component: () => import("./views/Community/AttendanceSheet.vue") },
+				{ path: "calendar", name: "community/calendar", component: () => import("./views/Community/Calendar.vue") },
+				{ path: "members", name: "community/members", component: () => import("./views/Community/Members.vue") },
+				{ path: "application", name: "community/application", component: () => import("./views/Community/Application.vue") },
+				{ path: "interview", name: "community/interview", component: () => import("./views/Community/Interview.vue") }
 			]
 		},
 		{
 			path: "/*",
 			name: "page404",
-			component: Page404
+			component: () => import("./views/Page404.vue")
 		}
 	]
 });
